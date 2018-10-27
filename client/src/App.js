8e72d4b9e39f985c90cdaa33cb6de3613d8eeb40
Mortifica <josh.hanke@gmail.com>
2018-10-27 15:38:02 -0500

    Added axion for http calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios'
+
 class App extends Component {
+  constructor () {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  handleClick() {
+    axios.get('https://api.github.com/users/TomWasHere')
+    .then(response => this.setState({username: response.data.name}))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='button_container'>
+        <button className='button' onClick={this.handleClick}>Click Me</button>
+        <p>{this.state.username}</p>
       </div>
     );
   }
